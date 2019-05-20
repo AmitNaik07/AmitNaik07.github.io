3b8f17a7a3d68abdfd46e5f7ea24ed3ce749c1c0
Amit Naik <mailamit70@gmail.com>
2019-05-20 13:08:34 -0400

    Add files via upload

---
+++ US/state_data.js
@@ -0,0 +1,82 @@
+var states = {
+  IA: {
+    name: "Iowa",
+    lived: "1995-03-01 to 2006-06-04",
+    description: "Taking it easy in the prairies"
+  },
+  FL: {
+    name: "Florida",
+    visited: "2017-010-16",
+    description: "Travel"
+  },
+    DC2: {
+    name: "Washington, District of Columbia",
+    visited: "2018-01-18",
+    description: "Travel"
+  },
+  	MD: {
+		name: "Maryland",
+		visited: "2018-08-22",
+		description: ""
+	},
+  	NY: {
+		name: "North Carolina",
+		visited: "2018-08-11",
+		description: "Visited Tejas and Nisha"
+	},
+  	PA: {
+		name: "Pennsylvania",
+		visited: "2018-10-15",
+    description: "Visited Sanchi @ Pittsburgh in 2018 "
+	},
+  KY: {
+    name: "Tennessee",
+    lived: "2018-05-20",
+    description: "Internship at Humana"
+  },
+  	TN: {
+		name: "Tennessee",
+		visited: "2018-08-20",
+    description: "Nashville trip with the interns"
+	},
+    IL: {
+    name: "Illinois",
+  	visited: "2003-02-07",
+    description: ""
+  },
+	  VA: {
+		name: "Virginia",
+		lived: "2017-08-15",
+    description: "Getting my Masters"
+	},
+  MO: {
+    name: "Missouri",
+    visited: "2002-11-18",
+    description: "Visited St. Louis"
+  },
+  MI: {
+    name: "Michigan",
+    visited: "2004-07-22",
+    description: "Visited Detroit"
+  },
+  WI: {
+    name: "Wisconson",
+    visited: "2003-06-02",
+    description: ""
+  },
+  NE: {
+    name: "Nebraska",
+    visited: "2003-08-10",
+    description: "Visited the Omaha zoo"
+  },
+  SD: {
+    name: "South Dakota",
+    visited: "2001-02-18",
+    description: "Visited Mount Rushmore"
+  },
+  GA: {
+    name: "Georgia",
+    visited: "2017-10-25",
+    description: "Visited Savannah"
+  }
+}
